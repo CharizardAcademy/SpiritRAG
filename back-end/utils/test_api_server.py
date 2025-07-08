b39e1685dc13d321e23851df04ef78e8229d724c
Yingqiang Gao <gaoyingqiang@live.cn>
2025-07-08 18:31:54 +0200

    Update test_api_server.py

--- back-end/utils/test_api_server.py
+++ back-end/utils/test_api_server.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # Define the API server URL
-API_BASE_URL = "http://127.0.0.1:5000/api"
+API_BASE_URL = "/to/your/api/endpoint/"
 
 def test_search_documents():
     """
